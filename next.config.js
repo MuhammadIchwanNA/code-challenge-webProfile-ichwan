0cf7fa6335787262eb18d58eedda70bfb1829404
unemployedMAXI <muhammadalam1636@gmail.com>
2025-08-26 11:33:53 +0700

    fix error in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    // You can add env variables here if needed
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
